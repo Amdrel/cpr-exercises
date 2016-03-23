5c7769f7f9fca3e82c9411cabae2eee80794d08f
Walter Kuppens <reshurum@gmail.com>
2016-03-23 09:56:34 -0700

    The good ol' switcheroo

--- 1.2/main.c
+++ 1.2/main.c
@@ -8,7 +8,7 @@ main() {
   upper = 300;
   step = 20;
 
-  farenheight = lower;
+  celsius = lower;
 
   // Table heading.
   printf("%11s %7s\n", "FARENHEIGHT", "CELSIUS");
@@ -16,8 +16,8 @@ main() {
   // Step through farenheight values between lower and upper with a specified
   // step value. Print the celsius conversion of those values.
   while (farenheight <= upper) {
-    celsius = (5.0 / 9.0) * (farenheight - 32.0);
+    farenheight = (celsius * (9.0 / 5.0)) + 32.0;
     printf("%11.0f %7.1f\n", farenheight, celsius);
-    farenheight = farenheight + step;
+    celsius = celsius + step;
   }
 }
