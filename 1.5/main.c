ce410074b4a854674a8b643cea3e9899c2144e54
Walter Kuppens <reshurum@gmail.com>
2016-03-23 11:39:35 -0700

    Screw multiple spaces
    There can only be one!

--- 1.5/main.c
+++ 1.5/main.c
@@ -2,21 +2,17 @@
 
 main() {
   int c = 0;
-  int newlines = 0;
-  int tabs = 0;
   int blanks = 0;
 
   while ((c = getchar()) != EOF) {
-    if (c == '\n') {
-      newlines++;
-    } else if (c == '\t') {
-      tabs++;
-    } else if (c == ' ') {
+    if (c == ' ') {
       blanks++;
+    } else {
+      blanks = 0;
+    }
+
+    if (blanks <= 1) {
+      putchar(c);
     }
   }
-
-  printf("Newline count: %d\n", newlines);
-  printf("Tab count: %d\n", tabs);
-  printf("Whitespace count: %d\n", blanks);
 }
