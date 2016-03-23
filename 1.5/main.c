c6a302466ed53082abb0422b4c92873b1321e8ff
Walter Kuppens <reshurum@gmail.com>
2016-03-23 12:24:17 -0700

    It's a word dumper
    Of what use is this? Who knows.

--- 1.5/main.c
+++ 1.5/main.c
@@ -1,17 +1,33 @@
 #include <stdio.h>
 
+#define IN  0
+#define OUT 1
+
 main() {
-  int c = 0;
+  int c, newlines, words, characters;
+  newlines = words = characters = 0;
+
+  int state = OUT;
 
   while ((c = getchar()) != EOF) {
-    if (c == '\t') {
-      printf("\\t");
-    } else if (c == '\b') {
-      printf("\\b");
-    } else if (c == '\\') {
-      printf("\\\\");
-    } else {
+    characters++;
+
+    if (c == '\n') {
+      newlines++;
+    }
+
+    if (c == ' ' || c == '\n' || c == '\t') {
+      state = OUT;
+    } else if (state == OUT) {
+      state = IN;
+      words++;
+      putchar('\n');
+    }
+
+    if (!(c == ' ' || c == '\n' || c == '\t')) {
       putchar(c);
     }
   }
+
+  printf("\n\n%d %d %d\n", newlines, words, characters);
 }
